4545f43ae964e14b96ff9318fe4a22e8d263a8a4
Nicholas Mirigliani <36427716+NobbyBop@users.noreply.github.com>
2024-04-18 12:46:50 -0400

    home route

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,15 @@
 import { Router } from "express";
+import { reviewData } from "../data/index.js";
 const router = Router();
 
-router.route("/").get(async (req, res) => {});
+router.route("/").get(async (req, res) => {
+    try{
+        let allReviews = await reviewData.getAllReviews()
+        return res.render("home",{title:"CaviarClub", reviews:allReviews})
+    } catch(e){
+        return res.status(500).json({error:"Internal server error."})
+    }
+    
+});
 
 export default router;
