7204433516a847e92cc1e982799feb83f0675cd1
Nicholas Mirigliani <36427716+NobbyBop@users.noreply.github.com>
2024-04-18 15:36:57 -0400

    Updated getAllReviews()

--- data/review/getAllReviews.js
+++ data/review/getAllReviews.js
@@ -1,4 +1,5 @@
 import {reviews} from "../../config/mongoCollections.js";
+import { userData, dishData } from "../index.js";
 import * as h from "../../helpers.js"
 
 export async function getAllReviews(skipNum, limitNum) {
@@ -8,18 +9,36 @@ export async function getAllReviews(skipNum, limitNum) {
     Could be useful for multiple pages.
     */
     let col = await reviews()
-
+    let allReviews = []
     try{
         skipNum = h.vInt(skipNum)
         limitNum = h.vInt(limitNum)
-        let allReviews = col.find({}).skip(skipNum).limit(limitNum).toArray()
-        return allReviews
+        allReviews = col.find({}).skip(skipNum).limit(limitNum).toArray()
     }
     catch(e){
-        let allReviews = col.find({}).toArray()
-        return allReviews
+        allReviews = col.find({}).toArray()
     }
-    
+    if(!allReviews) throw new Error("Could not get all reviews!")
+
+    /*
+    Most displaying of reviews requires username and dish name, so I
+    include them in the object here before returning. 
+    */
+    for(let review of allReviews){
+        try{
+            let user = await userData.getUserFromId(String(review.userId))
+            review.username = user.username
+        } catch(e) {
+            throw new Error("Invalid user data in review!")
+        }
+        try{
+            let dish = await userData.getDishFromId(String(review.dishId))
+            review.dishname = dish.name
+        } catch(e) {
+            throw new Error("Invalid dish data in review!")
+        }
+    }
+    return allReviews
 }
 
 
