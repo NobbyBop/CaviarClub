46e167c162ddf7a1b06f0057a68a966dfdded249
Nicholas Mirigliani <36427716+NobbyBop@users.noreply.github.com>
2024-04-18 12:31:38 -0400

    getAllReviews

--- data/review/getAllReviews.js
+++ data/review/getAllReviews.js
@@ -1,1 +1,9 @@
-export function getAllReviews() {}
+import {reviews} from "../../config/mongoCollections.js";
+export async function getAllReviews() {
+    let col = await reviews()
+    allReviews = col.find({})
+    return getAllReviews
+}
+
+
+
