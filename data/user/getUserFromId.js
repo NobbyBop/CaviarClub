64a70040d2b16db2ba2f6342a4689d7063587f63
Nicholas Mirigliani <36427716+NobbyBop@users.noreply.github.com>
2024-04-18 15:14:06 -0400

    getUserFromId()

--- data/user/getUserFromId.js
+++ data/user/getUserFromId.js
@@ -1,1 +1,11 @@
-export async function getUserFromId(id) {}
+import * as h from "../../helpers.js"
+import { users } from "../../config/mongoCollections"
+import { ObjectId } from "mongodb";
+
+export async function getUserFromId(id) {
+    id = h.vId(id)
+    let col = await users()
+    let user = await col.findOne({_id:new ObjectId()})
+    if(!user) throw new Error("Could not find user with that id!")
+    return user
+}
--- helpers.js
+++ helpers.js
@@ -5,3 +5,23 @@ export const vInt = (num) => {
     if(num !== num.toFixed(0)) throw new Error("Not an integer!")
     return num
 }
+
+export const vStr = (str) => {
+    if(str === undefined || typeof str !== "string" ||
+    str.trim().length === 0){
+        throw new Error("Invalid string.")
+    }
+    else {
+        return str.trim()
+    }
+}
+
+export const vId = (id) => {
+    try {
+        id = vStr(id)
+    } catch(e){
+        throw new Error("Error: Invalid object ID.")
+    }
+    if(!ObjectId.isValid(id)) throw new Error("Error: Invalid object ID.")
+    return id
+}
