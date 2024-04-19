479ceb0bfb6a157f598a54a3210fcb1533d9790f
Nicholas Mirigliani <36427716+NobbyBop@users.noreply.github.com>
2024-04-19 11:30:20 -0400

    taking a break now...

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,21 @@
 import { Router } from "express";
-import { reviewData } from "../data/index.js";
+import { reviewData, userData, dishData } from "../data/index.js";
 const router = Router();
 
 router.route("/").get(async (req, res) => {
     let allReviews = []
     try{
         let allReviews = await reviewData.getAllReviews(0, 20)
+        for(let r of allReviews){
+            try{
+                let user = userData.getUserFromId(String(r.userId))
+                let dish = dishData.getDishFromId(String(r.dishId))
+                r.username = user.name
+                r.dishname = dish.name
+            } catch(e) {
+                return res.status(500).json({error:"Internal server error."})
+            }
+        }
         return res.render("home",{title:"CaviarClub", reviews:allReviews})
     } catch(e){
         return res.status(500).json({error:"Internal server error."})
