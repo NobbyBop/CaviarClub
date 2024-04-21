525e7d3632210166c819138fba3d03c03f1d77fb
Andrew Yurovchak <andy@yurovchak.net>
2024-04-21 13:57:29 -0400

    / routes to  /home

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-app.use('/public', express.static('public'));
-app.use('/static', express.static('static'));
-
+app.use("/public", express.static("public"));
+app.use("/static", express.static("static"));
+app.use("/", (req, res, next) => {
+	if (req.path == "/") return res.redirect("/home");
+	next();
+});
 const rewriteUnsupportedBrowserMethods = (req, res, next) => {
-    if (req.body && req.body._method) {
-      req.method = req.body._method;
-      delete req.body._method;
-    }
-    next();
-  };
+	if (req.body && req.body._method) {
+		req.method = req.body._method;
+		delete req.body._method;
+	}
+	next();
+};
 app.use(rewriteUnsupportedBrowserMethods);
 
 configRoutes(app);
