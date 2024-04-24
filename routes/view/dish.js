e73b4a4fc9c402e51954a4a2602bd88464e57eda
Andrew Yurovchak <andy@yurovchak.net>
2024-04-24 11:54:35 -0400

    removed a console.log

--- routes/view/dish.js
+++ routes/view/dish.js
@@ -6,47 +6,46 @@ import { getReviewsFromDishId } from "../../data/review/getReviewsFromDishId.js"
 const router = Router();
 
 router.route("/:dishId").get(async (req, res) => {
-  let dishId;
-  try {
-    dishId = checkId(req.params.dishId);
-  } catch ({ message }) {
-    return res.render("error", { message });
-  }
-  console.log(dishId);
+	let dishId;
+	try {
+		dishId = checkId(req.params.dishId);
+	} catch ({ message }) {
+		return res.render("error", { message });
+	}
 
-  let dish;
-  try {
-    dish = await getDishFromId(dishId);
-  } catch ({ message }) {
-    return res.render("error", { message });
-  }
+	let dish;
+	try {
+		dish = await getDishFromId(dishId);
+	} catch ({ message }) {
+		return res.render("error", { message });
+	}
 
-  let restaurant;
-  try {
-    restaurant = await getRestaurantFromDishId(dishId);
-  } catch ({ message }) {
-    return res.render("error", { message });
-  }
+	let restaurant;
+	try {
+		restaurant = await getRestaurantFromDishId(dishId);
+	} catch ({ message }) {
+		return res.render("error", { message });
+	}
 
-  let reviews;
-  try {
-    reviews = await getReviewsFromDishId(dishId);
-  } catch ({ message }) {
-    return res.render("error", { message });
-  }
+	let reviews;
+	try {
+		reviews = await getReviewsFromDishId(dishId);
+	} catch ({ message }) {
+		return res.render("error", { message });
+	}
 
-  reviews.map((review) => {
-    if (review.content.length > 150)
-      review.content = `${review.content.substring(0, 150)}...`;
-  });
-  return res.render("view/dish", {
-    title: dish.name,
-    restaurantName: restaurant.name,
-    restaurantId: restaurant._id,
-    averageRating: dish.averageRating,
-    dishReviews: reviews,
-    dishId: dish._id,
-  });
+	reviews.map((review) => {
+		if (review.content.length > 150)
+			review.content = `${review.content.substring(0, 150)}...`;
+	});
+	return res.render("view/dish", {
+		title: dish.name,
+		restaurantName: restaurant.name,
+		restaurantId: restaurant._id,
+		averageRating: dish.averageRating,
+		dishReviews: reviews,
+		dishId: dish._id,
+	});
 });
 
 export default router;
