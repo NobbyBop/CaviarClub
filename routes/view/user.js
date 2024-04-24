8e9f9c1864aa695f6b106ba15d3fcac6f3cea735
tcheng529 <tcheng7@stevens.edu>
2024-04-24 12:05:56 -0400

    add user route

--- routes/view/user.js
+++ routes/view/user.js
@@ -1,6 +1,18 @@
 import { Router } from "express";
+import { getUserFromId } from "../../data/user/getUserFromId.js";
+import { getReviewsFromUser } from "../../data/user/getReviewsFromUser.js";
+
 const router = Router();
 
-router.route("/:userId").get(async (req, res) => {});
+router.get("/:userId", async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const user = await getUserFromId(userId);
+        const reviews = await getReviewsFromUser(userId);
+        res.render("user", { user, reviews });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
 export default router;
