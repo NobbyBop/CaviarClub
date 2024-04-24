106adc25f61f4886500405522c0f52278b403888
Andrew Yurovchak <andy@yurovchak.net>
2024-04-24 11:12:59 -0400

    view restaurant page

--- routes/view/restaurant.js
+++ routes/view/restaurant.js
@@ -1,6 +1,36 @@
 import { Router } from "express";
+import { checkId } from "../../helpers.js";
+import { getRestaurantFromId } from "../../data/restaurant/getRestaurantFromId.js";
+import { getDishesFromRestaurantId } from "../../data/dish/getDishesFromRestaurantId.js";
 const router = Router();
 
-router.route("/:restaurantId").get(async (req, res) => {});
+router.route("/:restaurantId").get(async (req, res) => {
+	let restaurantId;
+	try {
+		restaurantId = checkId(req.params.restaurantId.toString());
+	} catch ({ message }) {
+		return res.render("error", { message });
+	}
+
+	let restaurant;
+	try {
+		restaurant = await getRestaurantFromId(restaurantId);
+	} catch ({ message }) {
+		return res.render("error", { message });
+	}
+
+	let dishes;
+	try {
+		dishes = await getDishesFromRestaurantId(restaurantId);
+	} catch ({ message }) {
+		return res.render("error", { message });
+	}
+
+	return res.render("view/restaurant", {
+		title: `${restaurant.name} | Restaurant`,
+		restaurant,
+		dishes,
+	});
+});
 
 export default router;
