f0f1b2c2c40708c25c7e5aed6467e44bfcef3534
Andrew Yurovchak <andy@yurovchak.net>
2024-05-02 12:27:02 -0400

    small clientside validation fix

--- public/js/createNewDish.js
+++ public/js/createNewDish.js
@@ -1,8 +1,7 @@
 $("form").submit((e) => {
 	const data = new FormData(e.target);
 
-	for (const pair of Object.entries(data)) {
-		console.log(pair);
+	for (const pair of data.entries()) {
 		if (pair[1].trim().length == 0) {
 			e.preventDefault();
 			alert("all fields must be filled out...");
--- public/js/createNewRestaurant.js
+++ public/js/createNewRestaurant.js
@@ -1,7 +1,7 @@
 $("form").submit((e) => {
 	const data = new FormData(e.target);
 
-	for (const pair of Object.entries(data)) {
+	for (const pair of data.entries()) {
 		console.log(pair);
 		if (pair[1].trim().length == 0) {
 			e.preventDefault();
